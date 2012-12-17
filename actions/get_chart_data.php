973eba9c6960e18a26206f8c0f5e3d65b2a6d19c
Jerome Bakker <jeabakker@coldtrick.com>
2012-12-17 16:13:32 +0100

    rewrote to pagehandler

--- actions/get_chart_data.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-	if($chart_id = get_input("chart_id")){
-		$result = advanced_statistics_get_chart_data($chart_id);
-	}
-	
-	echo json_encode($result);
---
+++ lib/page_handlers.php
@@ -0,0 +1,30 @@
+<?php
+
+	function advanced_statistics_page_handler($page){
+		
+		switch($page[0]){
+			case "users":
+				echo advanced_statistics_get_users_data($page[1]);
+				break;
+			case "groups":
+				echo advanced_statistics_get_groups_data($page[1]);
+				break;
+			case "activity":
+				echo advanced_statistics_get_activity_data($page[1]);
+				break;
+			case "content":
+				echo advanced_statistics_get_content_data($page[1]);
+				break;
+			case "system":
+				echo advanced_statistics_get_system_data($page[1]);
+				break;
+			case "widgets":
+				echo advanced_statistics_get_widgets_data($page[1]);
+				break;
+			default:
+				return false;
+				break;
+		}
+		
+		return true;
+	}
