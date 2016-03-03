7afec2fa20eea35911b492e2321c24c0a004f18d
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-03-03 19:59:15 +0100

    French translation

---
+++ languages/fr.php
@@ -0,0 +1,67 @@
+<?php
+
+return array(
+	
+	// admin menu items
+	'admin:advanced_statistics' => 'Statistiques avancées',
+	'admin:advanced_statistics:users' => 'Membres',
+	'admin:advanced_statistics:groups' => 'Groupes',
+	'admin:advanced_statistics:content' => 'Publications',
+	'admin:advanced_statistics:activity' => 'Activité',
+	'admin:advanced_statistics:widgets' => 'Widgets',
+	'admin:advanced_statistics:system' => 'Système',
+
+	// users
+	'advanced_statistics:users:popular' => 'Membres populaires',
+	'advanced_statistics:users:most_used_domains' => 'Domaines des adresses emails les plus utilisés',
+	'advanced_statistics:users:account_creation' => 'Création des comptes',
+	'advanced_statistics:users:account_status' => 'Statut du compte',
+	'advanced_statistics:users:account_activity' => 'Activité du compte',
+	'advanced_statistics:users:language_distribution' => 'Répartition des langues',
+	'advanced_statistics:users:profile_field_usage' => 'Utilisation des champs du profil',
+	
+	// activity
+	'advanced_statistics:activity:day' => 'Activité quotidienne',
+	'advanced_statistics:activity:hour' => 'Activité horaire',
+	'advanced_statistics:activity:timeline' => 'Activité globale',
+	'advanced_statistics:activity:day:1' => 'Dimanche',
+	'advanced_statistics:activity:day:2' => 'Lundi',
+	'advanced_statistics:activity:day:3' => 'Mardi',
+	'advanced_statistics:activity:day:4' => 'Mercredi',
+	'advanced_statistics:activity:day:5' => 'Jeudi',
+	'advanced_statistics:activity:day:6' => 'Vendredi',
+	'advanced_statistics:activity:day:7' => 'Samedi',
+	
+	// widgets
+	'advanced_statistics:widgets:handlers' => 'Gestionnaires des widgets',
+	'advanced_statistics:widgets:context' => 'Contexte des widgets',
+
+	// content
+	'advanced_statistics:content:totals' => 'Totaux des publications',
+	'advanced_statistics:content:distribution' => 'Distribution des pubications dans les conteneurs',
+
+	// system
+	'advanced_statistics:system:files:users' => 'Membres avec le plus de fichiers et photos',
+	'advanced_statistics:system:files:groups' => 'Groupes avec le plus de fichiers et photos',
+	
+	// groups
+	'advanced_statistics:groups:popular' => "Groupes populaires",
+	'advanced_statistics:groups:popular_tools' => "Outils des groupes populaires",
+	'advanced_statistics:groups:most_active' => "Groupes les plus actifs (dernière semaine)",
+	'advanced_statistics:groups:least_active' => "Groupes les moins actifs",
+	'advanced_statistics:groups:dead_vs_alive' => "Inactif vs. Actif",
+	'advanced_statistics:groups:dead_vs_alive:last_month' => "< 1 mois [%d]",
+	'advanced_statistics:groups:dead_vs_alive:3_months' => "< 3 mois [%d]",
+	'advanced_statistics:groups:dead_vs_alive:6_months' => "< 6 mois [%d]",
+	'advanced_statistics:groups:dead_vs_alive:year' => "< 1 an [%d]",
+	'advanced_statistics:groups:dead_vs_alive:more_year' => "> 1 an [%d]",
+);
+
+		// widgets
+		'advanced_statistics:widgets:advanced_statistics:title' => "Statistiques avancées",
+		'advanced_statistics:widgets:advanced_statistics:description' => "Afficher quelques statistiques avancées",
+		'advanced_statistics:widgets:advanced_statistics:content:no_chart' => "Configurer le widget pour sélectionner un graphique.",
+		'advanced_statistics:widgets:advanced_statistics:content:no_jqplot' => "Veuillez rafraîchir la page pour afficher le contenu de ce widget.",
+		
+		'advanced_statistics:widgets:online_user_count:title' => "Compteur de membres connectés",
+		'advanced_statistics:widgets:online_user_count:description' => "Affiche le nombre de membres connectés et se rafraîchit automatiquement",
