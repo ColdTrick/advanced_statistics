764d0746872a3422d27e1926975dab9413ba99eb
Yaco <franco@reevo.org>
2017-01-15 19:16:45 -0300

    adds Spanish translation

---
+++ languages/es.php
@@ -0,0 +1,70 @@
+<?php
+
+$es = array(
+
+  // admin menu items
+  'admin:advanced_statistics' => 'Estadísticas Avanzadas',
+  'admin:advanced_statistics:users' => 'Usuarios',
+  'admin:advanced_statistics:groups' => 'Grupos',
+  'admin:advanced_statistics:content' => 'Contenido',
+  'admin:advanced_statistics:activity' => 'Actividad',
+  'admin:advanced_statistics:widgets' => 'Widgets',
+  'admin:advanced_statistics:system' => 'Sistema',
+
+  // users
+  'advanced_statistics:users:popular' => 'Usuarios Populares',
+  'advanced_statistics:users:most_used_domains' => 'Dominios de correo más usados',
+  'advanced_statistics:users:account_creation' => 'Creación de cuentas en el tiempo',
+  'advanced_statistics:users:account_status' => 'Estado de cuentas',
+  'advanced_statistics:users:account_activity' => 'Actividad de cuentas',
+  'advanced_statistics:users:language_distribution' => 'Distribución de lenguajes',
+  'advanced_statistics:users:profile_field_usage' => 'Uso del campo de perfil',
+
+  // activity
+  'advanced_statistics:activity:day' => 'Actividad por día',
+  'advanced_statistics:activity:hour' => 'Actividad por hora',
+  'advanced_statistics:activity:timeline' => 'Actividad en el tiempo',
+  'advanced_statistics:activity:day:1' => 'Domingo',
+  'advanced_statistics:activity:day:2' => 'Lunes',
+  'advanced_statistics:activity:day:3' => 'Martes',
+  'advanced_statistics:activity:day:4' => 'Miércoles',
+  'advanced_statistics:activity:day:5' => 'Jueves',
+  'advanced_statistics:activity:day:6' => 'Viernes',
+  'advanced_statistics:activity:day:7' => 'Sábado',
+
+  // widgets
+  'advanced_statistics:widgets:handlers' => 'Manipuladores de Widgets',
+  'advanced_statistics:widgets:context' => 'Contexto de Widgets',
+
+  // content
+  'advanced_statistics:content:totals' => 'Totales de contenido',
+  'advanced_statistics:content:distribution' => 'Distribución de contenedores de contenido',
+
+  // system
+  'advanced_statistics:system:files:users' => 'Usuarios con más archivos y fotos',
+  'advanced_statistics:system:files:groups' => 'Grupos con más archivos y fotos',
+
+  // groups
+  'advanced_statistics:groups:popular' => "Grupos populares",
+  'advanced_statistics:groups:popular_tools' => "Herramientas de grupo populares",
+  'advanced_statistics:groups:most_active' => "Grupos más activos (última semana)",
+  'advanced_statistics:groups:least_active' => "Grupos menos activos",
+  'advanced_statistics:groups:dead_vs_alive' => "Muertos vs. Vivos",
+  'advanced_statistics:groups:dead_vs_alive:last_month' => "< 1 mes [%d]",
+  'advanced_statistics:groups:dead_vs_alive:3_months' => "< 3 meses [%d]",
+  'advanced_statistics:groups:dead_vs_alive:6_months' => "< 6 meses [%d]",
+  'advanced_statistics:groups:dead_vs_alive:year' => "< 1 año [%d]",
+  'advanced_statistics:groups:dead_vs_alive:more_year' => "> 1 año [%d]",
+
+  // widgets
+  'advanced_statistics:widgets:advanced_statistics:title' => "Estadísticas avanzadas",
+  'advanced_statistics:widgets:advanced_statistics:description' => "Mostrar algunas estadísticas avanzadas",
+  'advanced_statistics:widgets:advanced_statistics:content:no_chart' => "Editar el widget para elegir un gráfico.",
+  'advanced_statistics:widgets:advanced_statistics:content:no_jqplot' => "Por favor actualiza la página para que este widget funcione.",
+
+  'advanced_statistics:widgets:online_user_count:title' => "Contador de usuarios en linea",
+  'advanced_statistics:widgets:online_user_count:description' => "Mostrar la cantidad de usuario en linea y actualizar automáticamente",
+
+);
+
+add_translation('es', $es);
