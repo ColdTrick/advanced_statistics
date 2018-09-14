8f9a4eadb0adefe72e69e47a68ed7e0a0a72109f
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-09-14 10:22:56 +0200

    chore: updated formatting of language file

--- languages/en.php
+++ languages/en.php
@@ -1,75 +1,73 @@
 <?php
 
-	$english = array(
+return [
 
-		// admin menu items
-		'admin:advanced_statistics' => 'Advanced Statistics',
-		'admin:advanced_statistics:users' => 'Users',
-		'admin:advanced_statistics:groups' => 'Groups',
-		'admin:advanced_statistics:content' => 'Content',
-		'admin:advanced_statistics:activity' => 'Activity',
-		'admin:advanced_statistics:widgets' => 'Widgets',
-		'admin:advanced_statistics:system' => 'System',
+	// admin menu items
+	'admin:advanced_statistics' => 'Advanced Statistics',
+	'admin:advanced_statistics:users' => 'Users',
+	'admin:advanced_statistics:groups' => 'Groups',
+	'admin:advanced_statistics:content' => 'Content',
+	'admin:advanced_statistics:activity' => 'Activity',
+	'admin:advanced_statistics:widgets' => 'Widgets',
+	'admin:advanced_statistics:system' => 'System',
+	
+	// date selection
+	'advanced_statistics:date_selection:title_button' => 'Show date selection',
+	'advanced_statistics:date_selection:start' => 'Start',
+	'advanced_statistics:date_selection:end' => 'End',
+
+	// users
+	'advanced_statistics:users:popular' => 'Popular Users',
+	'advanced_statistics:users:most_used_domains' => 'Most used email domains',
+	'advanced_statistics:users:account_creation' => 'Account creation over time',
+	'advanced_statistics:users:account_status' => 'Account status',
+	'advanced_statistics:users:account_activity' => 'Account activity',
+	'advanced_statistics:users:language_distribution' => 'Language distribution',
+	'advanced_statistics:users:profile_field_usage' => 'Profile field usage',
+
+	// activity
+	'advanced_statistics:activity:day' => 'Activity per day',
+	'advanced_statistics:activity:hour' => 'Activity per hour',
+	'advanced_statistics:activity:timeline' => 'Activity over time',
+	'advanced_statistics:activity:day:1' => 'Sunday',
+	'advanced_statistics:activity:day:2' => 'Monday',
+	'advanced_statistics:activity:day:3' => 'Tuesday',
+	'advanced_statistics:activity:day:4' => 'Wednesday',
+	'advanced_statistics:activity:day:5' => 'Thursday',
+	'advanced_statistics:activity:day:6' => 'Friday',
+	'advanced_statistics:activity:day:7' => 'Saturday',
+
+	// widgets
+	'advanced_statistics:widgets:handlers' => 'Widget handlers',
+	'advanced_statistics:widgets:context' => 'Widget context',
+
+	// content
+	'advanced_statistics:content:totals' => 'Content totals',
+	'advanced_statistics:content:distribution' => 'Content container distribution',
+
+	// system
+	'advanced_statistics:system:files:users' => 'Users with the most files and photos',
+	'advanced_statistics:system:files:groups' => 'Groups with the most files and photos',
+
+	// groups
+	'advanced_statistics:groups:popular' => "Popular groups",
+	'advanced_statistics:groups:popular_tools' => "Popular group tools",
+	'advanced_statistics:groups:most_active' => "Most active groups (last week)",
+	'advanced_statistics:groups:least_active' => "Least active groups",
+	'advanced_statistics:groups:dead_vs_alive' => "Dead vs. Alive",
+	'advanced_statistics:groups:dead_vs_alive:last_month' => "< 1 month [%d]",
+	'advanced_statistics:groups:dead_vs_alive:3_months' => "< 3 months [%d]",
+	'advanced_statistics:groups:dead_vs_alive:6_months' => "< 6 months [%d]",
+	'advanced_statistics:groups:dead_vs_alive:year' => "< 1 year [%d]",
+	'advanced_statistics:groups:dead_vs_alive:more_year' => "> 1 year [%d]",
+
+	// widgets
+	'advanced_statistics:widgets:advanced_statistics:title' => "Advanced Statistics",
+	'advanced_statistics:widgets:advanced_statistics:description' => "Show some advanced statistics",
+	'advanced_statistics:widgets:advanced_statistics:content:no_chart' => "Edit the widget to select a chart.",
+	'advanced_statistics:widgets:advanced_statistics:content:no_jqplot' => "Please refresh the page to make this widget work.",
+	
+	'advanced_statistics:widgets:online_user_count:title' => "Online Users Counter",
+	'advanced_statistics:widgets:online_user_count:description' => "Show the amount of online users and will automatically refresh",
 		
-		// date selection
-		'advanced_statistics:date_selection:title_button' => 'Show date selection',
-		'advanced_statistics:date_selection:start' => 'Start',
-		'advanced_statistics:date_selection:end' => 'End',
-
-		// users
-		'advanced_statistics:users:popular' => 'Popular Users',
-		'advanced_statistics:users:most_used_domains' => 'Most used email domains',
-		'advanced_statistics:users:account_creation' => 'Account creation over time',
-		'advanced_statistics:users:account_status' => 'Account status',
-		'advanced_statistics:users:account_activity' => 'Account activity',
-		'advanced_statistics:users:language_distribution' => 'Language distribution',
-		'advanced_statistics:users:profile_field_usage' => 'Profile field usage',
-
-		// activity
-		'advanced_statistics:activity:day' => 'Activity per day',
-		'advanced_statistics:activity:hour' => 'Activity per hour',
-		'advanced_statistics:activity:timeline' => 'Activity over time',
-		'advanced_statistics:activity:day:1' => 'Sunday',
-		'advanced_statistics:activity:day:2' => 'Monday',
-		'advanced_statistics:activity:day:3' => 'Tuesday',
-		'advanced_statistics:activity:day:4' => 'Wednesday',
-		'advanced_statistics:activity:day:5' => 'Thursday',
-		'advanced_statistics:activity:day:6' => 'Friday',
-		'advanced_statistics:activity:day:7' => 'Saturday',
-
-		// widgets
-		'advanced_statistics:widgets:handlers' => 'Widget handlers',
-		'advanced_statistics:widgets:context' => 'Widget context',
-
-		// content
-		'advanced_statistics:content:totals' => 'Content totals',
-		'advanced_statistics:content:distribution' => 'Content container distribution',
-
-		// system
-		'advanced_statistics:system:files:users' => 'Users with the most files and photos',
-		'advanced_statistics:system:files:groups' => 'Groups with the most files and photos',
-
-		// groups
-		'advanced_statistics:groups:popular' => "Popular groups",
-		'advanced_statistics:groups:popular_tools' => "Popular group tools",
-		'advanced_statistics:groups:most_active' => "Most active groups (last week)",
-		'advanced_statistics:groups:least_active' => "Least active groups",
-		'advanced_statistics:groups:dead_vs_alive' => "Dead vs. Alive",
-		'advanced_statistics:groups:dead_vs_alive:last_month' => "< 1 month [%d]",
-		'advanced_statistics:groups:dead_vs_alive:3_months' => "< 3 months [%d]",
-		'advanced_statistics:groups:dead_vs_alive:6_months' => "< 6 months [%d]",
-		'advanced_statistics:groups:dead_vs_alive:year' => "< 1 year [%d]",
-		'advanced_statistics:groups:dead_vs_alive:more_year' => "> 1 year [%d]",
-
-		// widgets
-		'advanced_statistics:widgets:advanced_statistics:title' => "Advanced Statistics",
-		'advanced_statistics:widgets:advanced_statistics:description' => "Show some advanced statistics",
-		'advanced_statistics:widgets:advanced_statistics:content:no_chart' => "Edit the widget to select a chart.",
-		'advanced_statistics:widgets:advanced_statistics:content:no_jqplot' => "Please refresh the page to make this widget work.",
-		
-		'advanced_statistics:widgets:online_user_count:title' => "Online Users Counter",
-		'advanced_statistics:widgets:online_user_count:description' => "Show the amount of online users and will automatically refresh",
-			
-	);
-
-	add_translation('en', $english);
+];
