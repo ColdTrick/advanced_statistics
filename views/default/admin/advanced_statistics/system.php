41dee39ec6256bd2338563aac04e67f8dc0d334e
Jeroen Dalsem <jdalsem@coldtrick.com>
2019-02-13 18:03:27 +0100

    chore: removed unused code

--- views/default/admin/advanced_statistics/system.php
+++ views/default/admin/advanced_statistics/system.php
@@ -7,13 +7,8 @@ FileStorage
 
     most consuming owners (bar)
     most consuming groups (bar)
-    most consuming sites (bar)
     Based on ElggFile and ElggPhoto (tidypics) stacked bar
 
-
-DB Size
-
-    data distribution (bar)
 */
 
 echo elgg_view('advanced_statistics/elements/chart', [
@@ -31,15 +26,3 @@ echo elgg_view('advanced_statistics/elements/chart', [
 	'section' => 'system',
 	'chart' => 'files_groups',
 ]);
-
-// $title = elgg_echo('advanced_statistics:system:files:sites');
-// $body = "<div id='advanced-statistics-system-files-sites' class='advanced-statistics-plot-container'></div><div class='elgg-ajax-loader'></div>";
-// echo elgg_view_module("inline", $title, $body);
-
-// $title = elgg_echo('advanced_statistics:system:database:tables');
-// $body = "<div id='advanced-statistics-system-database-tables' class='advanced-statistics-plot-container'></div><div class='elgg-ajax-loader'></div>";
-// echo elgg_view_module("inline", $title, $body);
-
-// $title = elgg_echo('advanced_statistics:system:database:overhead');
-// $body = "<div id='advanced-statistics-system-database-overhead' class='advanced-statistics-plot-container'></div><div class='elgg-ajax-loader'></div>";
-// echo elgg_view_module("inline", $title, $body);
