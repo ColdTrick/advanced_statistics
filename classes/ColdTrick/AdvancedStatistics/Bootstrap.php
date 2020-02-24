1b19fa14fb8b73b9cd74b41ddb5d9c271a4d739b
Jeroen Dalsem <jdalsem@coldtrick.com>
2020-02-24 13:08:10 +0100

    fixed: make sure css can be loaded

--- classes/ColdTrick/AdvancedStatistics/Bootstrap.php
+++ classes/ColdTrick/AdvancedStatistics/Bootstrap.php
@@ -14,6 +14,8 @@ class Bootstrap extends DefaultPluginBootstrap {
 		elgg_register_ajax_view('advanced_statistics/admin_data');
 		elgg_register_ajax_view('advanced_statistics/group_data');
 		
+		elgg_register_simplecache_view('css/advanced_statistics/jqplot');
+		
 		elgg_register_plugin_hook_handler('register', 'menu:owner_block', '\ColdTrick\AdvancedStatistics\Menus::registerGroupItems');
 		elgg_register_plugin_hook_handler('register', 'menu:page', '\ColdTrick\AdvancedStatistics\Menus::registerAdminItems');
 	}
