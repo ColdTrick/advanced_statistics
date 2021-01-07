16e2ac52660e34e422201cb5a2db903e97d4fedd
Jerome Bakker <jeabakker@coldtrick.com>
2021-01-07 13:19:43 +0100

    added: non searchable content totals graph

--- views/default/admin/advanced_statistics/content.php
+++ views/default/admin/advanced_statistics/content.php
@@ -10,12 +10,21 @@
 echo elgg_view('advanced_statistics/date_selector');
 
 echo elgg_view('advanced_statistics/elements/chart', [
-	'title' => elgg_echo('advanced_statistics:content:totals'),
+	'title' => elgg_echo('advanced_statistics:content:totals') . ' - '. elgg_echo('admin:statistics:numentities:searchable'),
 	'id' => 'advanced-statistics-content-totals',
 	'date_limited' => true,
 	'page' => 'admin_data',
 	'section' => 'content',
 	'chart' => 'totals',
+]);
+
+echo elgg_view('advanced_statistics/elements/chart', [
+	'title' => elgg_echo('advanced_statistics:content:totals') . ' - '. elgg_echo('admin:statistics:numentities:other'),
+	'id' => 'advanced-statistics-content-totals-others',
+	'date_limited' => true,
+	'page' => 'admin_data',
+	'section' => 'content',
+	'chart' => 'totals-others',
 ]);
 
 echo elgg_view('advanced_statistics/elements/chart', [
---
+++ views/json/advanced_statistics/admin/content/totals-others.php
@@ -0,0 +1,46 @@
+<?php
+
+use Elgg\Database\Select;
+
+$result = [
+	'options' => advanced_statistics_get_default_chart_options('bar'),
+];
+
+$qb = Select::fromTable('entities', 'e');
+$qb->select('e.subtype as subtype');
+$qb->addSelect('count(*) AS total');
+$qb->where("e.type = 'object'");
+$qb->andWhere($qb->compare('e.subtype', '!=', get_registered_entity_types('object'), ELGG_VALUE_STRING));
+$qb->groupBy('e.subtype');
+$qb->orderBy('total', 'desc');
+
+$ts_limit = advanced_statistics_get_timestamp_query_part('e.time_created');
+if ($ts_limit) {
+	$qb->andWhere($ts_limit);
+}
+
+$query_result = $qb->execute()->fetchAll();
+
+$data = [];
+if ($query_result) {
+	foreach ($query_result as $row) {
+		$data[] = [
+			elgg_language_key_exists("item:object:{$row->subtype}") ? elgg_echo("item:object:{$row->subtype}") : ucfirst($row->subtype),
+			(int) $row->total,
+		];
+	}
+}
+$result['data'] = [$data];
+
+$result['options']['seriesDefaults']['rendererOptions'] = ['varyBarColor' => true];
+			
+$result['options']['highlighter'] = [
+	'show' => true,
+	'sizeAdjust' => 7.5,
+	'tooltipAxes' => 'y',
+];
+$result['options']['axes']['xaxis']['tickRenderer'] = '$.jqplot.CanvasAxisTickRenderer';
+$result['options']['axes']['xaxis']['tickOptions'] = ['angle' => '-30', 'fontSize' => '8pt'];
+
+echo json_encode($result);
+		
