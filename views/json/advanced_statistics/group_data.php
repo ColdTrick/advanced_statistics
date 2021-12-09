6b590ed0bcb963eab8e35b7faed4809cefa972e0
Jerôme Bakker <jeabakker@coldtrick.com>
2021-12-09 10:15:05 +0100

    chore: protect group data endpoint

--- views/json/advanced_statistics/group_data.php
+++ views/json/advanced_statistics/group_data.php
@@ -1,6 +1,17 @@
 <?php
 
 use Elgg\Exceptions\Http\BadRequestException;
+use Elgg\Exceptions\Http\EntityPermissionsException;
+
+$container_guid = (int) elgg_extract('container_guid', $vars);
+if (empty($container_guid)) {
+	throw new BadRequestException();
+}
+
+$group = get_entity($container_guid);
+if (!$group instanceof ElggGroup || !$group->canEdit()) {
+	throw new EntityPermissionsException();
+}
 
 $section = elgg_extract('section', $vars);
 $chart = elgg_extract('chart', $vars);
