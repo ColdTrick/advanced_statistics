4da9e1597f4a7350e0adaab04fd5b9d8d8f52038
Jerôme Bakker <jeabakker@coldtrick.com>
2021-12-16 09:01:11 +0100

    chore: wrapping up v5.1

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -6,7 +6,7 @@ use Elgg\Router\Middleware\GroupPageOwnerCanEditGatekeeper;
 
 return [
 	'plugin' => [
-		'version' => '5.0',
+		'version' => '5.1',
 	],
 	'settings' => [
 		'enable_group_stats' => 'no',
