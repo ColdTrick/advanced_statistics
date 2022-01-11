9c5726a29367dd9917b6282dded4b94f98b05881
Jeroen Dalsem <jdalsem@coldtrick.com>
2022-01-11 14:48:08 +0100

    changed: use new Elgg 4.1 field layout options

--- views/default/forms/advanced_statistics/date_selector.php
+++ views/default/forms/advanced_statistics/date_selector.php
@@ -7,12 +7,14 @@ echo elgg_view_field([
 		[
 			'#type' => 'date',
 			'#label' => elgg_echo('advanced_statistics:date_selection:start'),
+			'#class' => 'elgg-field-horizontal',
 			'name' => 'ts_lower',
 			'value' => get_input('ts_lower'),
 		],
 		[
 			'#type' => 'date',
 			'#label' => elgg_echo('advanced_statistics:date_selection:end'),
+			'#class' => 'elgg-field-horizontal',
 			'name' => 'ts_upper',
 			'value' => get_input('ts_upper'),
 		],
