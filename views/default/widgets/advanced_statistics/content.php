6b162bbb045d65bceb44f42c36324f31c902341e
Jerôme Bakker <jeabakker@coldtrick.com>
2022-12-08 14:56:12 +0100

    fixed: admin widget didn't show chart data
    and had an Ajax error

--- views/default/widgets/advanced_statistics/content.php
+++ views/default/widgets/advanced_statistics/content.php
@@ -13,8 +13,12 @@ elgg_require_js('advanced_statistics/charts');
 elgg_require_css('advanced_statistics/jqplot');
 
 list($id, $text) = explode('|', $chart);
+list($section, $chart_src) = explode(':', $text);
 
 echo elgg_view('advanced_statistics/elements/chart', [
 	'title' => elgg_echo('advanced_statistics:' . $text),
 	'id' => 'advanced-statistics-' . $id,
+	'page' => 'admin_data',
+	'section' => $section,
+	'chart' => $chart_src,
 ]);
